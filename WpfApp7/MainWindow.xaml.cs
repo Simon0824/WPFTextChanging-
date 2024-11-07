9760715df570c6054fe1292ab7fc713fd01b6ef2
Szymon Szostek <szymon.szostek008@gmail.com>
2024-11-07 13:20:21 +0100

    dodanie działania strony przy wybraniu(niedokończone)

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Edytor
             if (bold == null || italic == null || Podkresl == null || Czarnyczcionk == null || Czerwonyczcionk == null ||
                 Zielonyczcionk == null || Niebieskiczcionk == null ||
                 BezowyTlo == null || CzerwonyTlo == null || ZielonyTlo == null 
-                || NiebieskiTlo == null || Lewo == null || Srodek == null || Prawo == null || Arial == null ||
+                || NiebieskiTlo == null || Lewo == null || Srodek == null || Prawo == null || Just == null || Arial == null ||
                 Times == null || Courier == null)
             {
                 return; 
@@ -32,29 +32,91 @@ namespace Edytor
             UstawKolorCzcionki();
             UstawKolorTla();
             UstawJustowanie();
-            UstawKrojCzcionki();
+            UstawStylCzcionki();
             RozmCzionki();
             Progres();
         }
 
         private void UstawKolorCzcionki()
         {
-
+            if (Czarnyczcionk.IsChecked == true)
+            {
+                poletekstu.Foreground = Brushes.Black;
+            }
+            if (Czerwonyczcionk.IsChecked == true)
+            {
+                poletekstu.Foreground = Brushes.Red;
+            }
+            if (Zielonyczcionk.IsChecked == true)
+            {
+                poletekstu.Foreground = Brushes.Green;
+            }
+            if (Niebieskiczcionk.IsChecked == true)
+            {
+                poletekstu.Foreground = Brushes.Blue;
+            }
         }
 
         private void UstawKolorTla()
         {
-
+            if (BezowyTlo.IsChecked == true)
+            {
+                poletekstu.Background = Brushes.DarkKhaki;
+            }
+            if (CzerwonyTlo.IsChecked == true)
+            {
+                poletekstu.Background = Brushes.Red;
+            }
+            if (ZielonyTlo.IsChecked == true)
+            {
+                poletekstu.Background = Brushes.Green;
+            }
+            if (NiebieskiTlo.IsChecked == true)
+            {
+                poletekstu.Background = Brushes.Blue;
+            }
         }
 
         private void UstawJustowanie()
         {
+            if(Lewo.IsChecked == true)
+            poletekstu.TextAlignment = TextAlignment.Left;
+            if(Srodek.IsChecked==true)
+                poletekstu.TextAlignment= TextAlignment.Center;
+            if (Prawo.IsChecked == true)
+                poletekstu.TextAlignment = TextAlignment.Right;
+            if (Just.IsChecked == true)
+                poletekstu.TextAlignment = TextAlignment.Justify;
+            
+        }
 
+        private void UstawStylCzcionki()
+        {
+            //Domyślny styl tesktu
+            var styl = FontStyles.Normal;
+            var grubosc = FontWeights.Normal;
+            var dodatki = new TextDecorationCollection();
+
+            if(bold.IsChecked == true)
+                grubosc = FontWeights.Bold;
+            if(italic.IsChecked == true)
+                styl = FontStyles.Italic;
+            if (Podkresl.IsChecked == true)
+                dodatki = TextDecorations.Underline;
+            poletekstu.FontStyle = styl;
+            poletekstu.FontWeight = grubosc;
+            poletekstu.TextDecorations = dodatki;
         }
 
         private void UstawKrojCzcionki()
         {
-
+            if(Arial.IsChecked == true)
+            poletekstu.FontFamily = Arial.FontFamily;
+            if(Courier.IsChecked == true)
+                poletekstu.FontFamily = Courier.FontFamily;
+            if(Times.IsChecked == true)
+                poletekstu.FontFamily = Times.FontFamily;
+           
         }
 
         private void RozmCzionki()
@@ -64,7 +126,16 @@ namespace Edytor
 
         private void Progres()
         {
+            int progresik = 0;
 
+            if (bold.IsChecked == true) progresik++;
+            if (italic.IsChecked == true) progresik++;
+            if (Podkresl.IsChecked == true) progresik++;
+            if(Czarnyczcionk.IsChecked == true || Czerwonyczcionk.IsChecked == true ||   Zielonyczcionk.IsChecked == true
+                || Niebieskiczcionk.IsChecked == true) progresik++;
+            if (BezowyTlo.IsChecked == true || CzerwonyTlo.IsChecked == true || ZielonyTlo.IsChecked == true || NiebieskiTlo.IsChecked == true) progresik++;
+            if (Lewo.IsChecked == true || Srodek.IsChecked ==true || Prawo.IsChecked == true || Just.IsChecked == true) progresik++;
+            if ()
         }
     }
 }
