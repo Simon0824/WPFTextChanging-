70fa11e0eac4e2b3a551307e226a1d8a5213ed7e
767Bartek <barteklidzbarski9@gmail.com>
2024-11-07 10:25:22 +0100

    Dodanie justowania, aktualizacji formatowania telstu

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -9,11 +9,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp7
+namespace Edytor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,9 +19,52 @@ namespace WpfApp7
             InitializeComponent();
         }
 
-        private void InitializeComponent()
+        private void Aktualizuj(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (bold == null || italic == null || Podkresl == null || Czarnyczcionk == null || Czerwonyczcionk == null ||
+                Zielonyczcionk == null || Niebieskiczcionk == null ||
+                BezowyTlo == null || CzerwonyTlo == null || ZielonyTlo == null 
+                || NiebieskiTlo == null || Lewo == null || Srodek == null || Prawo == null || Arial == null ||
+                Times == null || Courier == null)
+            {
+                return; 
+            }
+            UstawKolorCzcionki();
+            UstawKolorTla();
+            UstawJustowanie();
+            UstawKrojCzcionki();
+            RozmCzionki();
+            Progres();
+        }
+
+        private void UstawKolorCzcionki()
+        {
+
+        }
+
+        private void UstawKolorTla()
+        {
+
+        }
+
+        private void UstawJustowanie()
+        {
+
+        }
+
+        private void UstawKrojCzcionki()
+        {
+
+        }
+
+        private void RozmCzionki()
+        {
+
+        }
+
+        private void Progres()
+        {
+
         }
     }
 }
